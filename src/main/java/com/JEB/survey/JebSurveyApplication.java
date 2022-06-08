4ae0a78d80bbdcc2ddb1dc06694787743d22612e
HuiEun-Lim <heyho930@gmail.com>
2022-06-08 13:26:30 +0900

    [임희은] 기본 프로젝트 세팅

---
+++ src/main/java/com/JEB/survey/JebSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.JEB.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JebSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JebSurveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JEB/survey/JebSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JEB.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JebSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
