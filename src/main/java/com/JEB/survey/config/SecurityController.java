d9aa088b04e17d66cf2594e8c9602c94aee17221
Bonnie <taehyeongim59@gmail.com>
2022-06-09 14:04:35 +0900

    [김태현] 기본 로그인 화면 접근 제어

--- src/main/java/com/JEB/survey/config/SecurityController.java
+++ src/main/java/com/JEB/survey/config/SecurityController.java
@@ -1,5 +1,13 @@
 package com.JEB.survey.config;
 
-public class SecurityController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class SecurityController {
+	 @RequestMapping("/")
+	  public String home(ModelAndView mav) {
+	    return "/member/home";  
+	  }
 }
