282b98ce3960c56c99a7d2d4cb10a1c6c39f37c9
HuiEun-Lim <heyho930@gmail.com>
2022-06-09 13:49:05 +0900

    [임희은] controller 패키지 변경

---
+++ src/main/java/com/JEB/survey/controller/ex_controller.java
@@ -0,0 +1,25 @@
+package com.JEB.survey.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ex_controller {
+
+	@RequestMapping(value = "/hello", method=RequestMethod.GET)
+	public ModelAndView hello(HttpServletRequest request) {
+		ModelAndView mv = new ModelAndView();
+		
+		mv.addObject("msg", "Hello Jetty");
+		
+		mv.setViewName("ex_list");
+		
+		return mv;
+	}
+	
+	
+}
