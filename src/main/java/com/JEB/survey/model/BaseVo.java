18e83f4d8b735c58c1e92e360df7f0723be7644c
HuiEun-Lim <heyho930@gmail.com>
2022-06-10 10:05:35 +0900

    [임희은] BaseVo 생성

---
+++ src/main/java/com/JEB/survey/model/BaseVo.java
@@ -0,0 +1,15 @@
+package com.JEB.survey.model;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BaseVo {
+	// 등록자 ID
+	private String regId;
+	// 등록 날짜
+	private Date regDate;
+	// 최종 수정 날짜
+	private Date modDate;
+}
---
+++ src/main/java/com/JEB/survey/model/SurveyDto.java
@@ -0,0 +1,14 @@
+package com.JEB.survey.model;
+
+public class SurveyDto extends BaseVo {
+	// 설문 조사 번호
+	private int survNo;
+	// 설문 제목
+	private String survTitle;
+	// 설문 설명
+	private String survDesc;
+	// 사용 여부
+	private String useYn;
+	// 삭제 여부
+	private String delYn;
+}
