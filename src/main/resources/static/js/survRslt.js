b4aa922e0f64eec7ee191627f1037e0839ebb276
HuiEun-Lim <heyho930@gmail.com>
2022-06-16 14:36:39 +0900

    [임희은] 설문 결과 페이지 chart js 및 목록으로 결과 출력

--- src/main/resources/static/js/survRslt.js
+++ src/main/resources/static/js/survRslt.js
@@ -1,91 +1,103 @@
 $(document).ready(function(){
-	
-	console.log(qustList);
-	
 	for(let idx in qustList) {
-		console.log("qustNo: " + qustList[idx].qustNo);
-		console.log("qustType: " + qustList[idx].qustType);
 		
-		if(qustList[idx].qustType != "long") {
+		let qust = qustList[idx];
+		let qustType = qust.qustType;
+		
+		if(qustType != "long" && qustType != "short") {
 			const ctx = document.getElementById(qustList[idx].qustNo).getContext('2d');
-			const myChart = new Chart(ctx, {
-			    type: 'bar',
-			    data: {
-			        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-			        datasets: [{
-			            label: '# of Votes',
-			            data: [12, 19, 3, 5, 2, 3],
-			            backgroundColor: [
-			                'rgba(255, 99, 132, 0.2)',
-			                'rgba(54, 162, 235, 0.2)',
-			                'rgba(255, 206, 86, 0.2)',
-			                'rgba(75, 192, 192, 0.2)',
-			                'rgba(153, 102, 255, 0.2)',
-			                'rgba(255, 159, 64, 0.2)'
-			            ],
-			            borderColor: [
-			                'rgba(255, 99, 132, 1)',
-			                'rgba(54, 162, 235, 1)',
-			                'rgba(255, 206, 86, 1)',
-			                'rgba(75, 192, 192, 1)',
-			                'rgba(153, 102, 255, 1)',
-			                'rgba(255, 159, 64, 1)'
-			            ],
-			            borderWidth: 1
-			        }]
-			    },
-			    options: {
-			        scales: {
-			            y: {
-			                beginAtZero: true
-			            }
-			        }
-			    }
-			});
-		}
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-	/* smaple chart js */
-	const ctx = document.getElementById('myChart').getContext('2d');
-	const myChart = new Chart(ctx, {
-	    type: 'bar',
-	    data: {
-	        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-	        datasets: [{
-	            label: '# of Votes',
-	            data: [12, 19, 3, 5, 2, 3],
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ],
-	            borderColor: [
-	                'rgba(255, 99, 132, 1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
-	            ],
-	            borderWidth: 1
-	        }]
-	    },
-	    options: {
-	        scales: {
-	            y: {
-	                beginAtZero: true
-	            }
-	        }
-	    }
-	});
-});
+			
+			let labels = [];
+			let data = [];
+			
+			for(let answ in qust.answerList) {
+				labels.push(qust.answerList[answ].answCont);
+				data.push(qust.answerList[answ].count);
+			}
+
+			if(qustType == "check") {
+				const barChart = new Chart(ctx, {
+				    type: 'bar',
+				    data: {
+				        labels: labels,
+				        datasets: [{
+				            label: qust.qustCont,
+				            data: data,
+				            backgroundColor: [
+				                'rgba(255, 99, 132, 0.4)',
+				                'rgba(54, 162, 235, 0.4)',
+				                'rgba(255, 206, 86, 0.4)',
+				                'rgba(75, 192, 192, 0.4)',
+				                'rgba(153, 102, 255, 0.4)',
+				                'rgba(255, 159, 64, 0.4)',
+				                'rgba(201, 203, 207, 0.4)'
+				            ],
+				            borderColor: [
+				                'rgba(255, 99, 132)',
+				                'rgba(54, 162, 235)',
+				                'rgba(255, 206, 86)',
+				                'rgba(75, 192, 192)',
+				                'rgba(153, 102, 255)',
+				                'rgba(255, 159, 64)',
+				                'rgb(201, 203, 207)'
+				            ],
+				            borderWidth: 1
+				        }]
+				    },
+				    options: {
+				        scales: {
+				            y: {
+				                beginAtZero: true,
+				                ticks: {
+									stepSize : 1,
+								}
+				            },
+				        }
+				    }
+				});
+			} else if(qustType == "select" || qustType == "radio") {
+				const pieChart = new Chart(ctx, {
+				    type: 'pie',
+				    data: {
+				        labels: labels,
+				        datasets: [{
+				            data: data,
+				            backgroundColor: [
+				            	'rgba(255, 99, 132, 0.4)',
+				                'rgba(54, 162, 235, 0.4)',
+				                'rgba(255, 206, 86, 0.4)',
+				                'rgba(75, 192, 192, 0.4)',
+				                'rgba(153, 102, 255, 0.4)',
+				                'rgba(255, 159, 64, 0.4)',
+				                'rgba(201, 203, 207, 0.4)'
+				            ],
+				            borderColor: [
+				                'rgba(255, 99, 132)',
+				                'rgba(54, 162, 235)',
+				                'rgba(255, 206, 86)',
+				                'rgba(75, 192, 192)',
+				                'rgba(153, 102, 255)',
+				                'rgba(255, 159, 64)',
+				                'rgb(201, 203, 207)'
+				            ],
+				            borderWidth: 1,
+				            hoverOffset: 4
+				        }]
+				    },
+				    options: {
+				    	legend: {
+				            position: 'right',
+				            display: true
+				        },
+				        plugins: {
+				            legend: {
+				                position: 'right',
+				            }
+				        }
+				    }
+				});
+				
+			} // 질문 유형에 따라 chart 그리는 if문
+		} // 장문/단답 확인용 if문
+	} // qustList
+}); // $(document).ready
